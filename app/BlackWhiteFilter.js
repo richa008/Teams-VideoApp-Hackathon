89961348a518b1f37be64cffa593c9f21bd83b49
Richa Deshmukh <rideshmu@microsoft.com>
2021-10-13 13:39:53 -0700

    Update

--- app/BlackWhiteFilter.js
+++ app/BlackWhiteFilter.js
@@ -8,7 +8,6 @@ class BlackWhiteFilter extends ImageFilter {
     getFragmentShader() {
       return `
       uniform sampler2D texture;
-      uniform float amount;
       varying vec2 texCoord;
       float rand(vec2 co) {
           return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
