8a9d387e58e0b7bee672939cf929792f10b0023d
Richa Deshmukh <rideshmu@microsoft.com>
2021-10-13 15:54:58 -0700

    Adding new filter

--- app/BlackWhiteFilter.js
+++ app/BlackWhiteFilter.js
@@ -1,13 +1,6 @@
 class BlackWhiteFilter extends ImageFilter {
   constructor() {
     super();
-    this.texture = gl.createTexture();
-
-    gl.bindTexture(gl.TEXTURE_2D, this.texture);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);;
   }
 
   getFragmentShader() {
