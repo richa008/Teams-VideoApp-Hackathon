c54c675da916e9d70cee2f8cf81889d3f5c2921a
Richa Deshmukh <rideshmu@microsoft.com>
2021-10-13 15:46:23 -0700

    Adding new filter

--- app/BlackWhiteFilter.js
+++ app/BlackWhiteFilter.js
@@ -3,7 +3,7 @@ class BlackWhiteFilter extends ImageFilter {
     super();
     this.texture = gl.createTexture();
 
-    gl.bindTexture(gl.TEXTURE_2D, _sourceTexture);
+    gl.bindTexture(gl.TEXTURE_2D, this.texture);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
@@ -19,7 +19,7 @@ class BlackWhiteFilter extends ImageFilter {
           return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);\
       }\
       void main() {\
-          vec4 color = texture2D(textureCurve, v_texCoord);\
+          vec4 color = texture2D(texture, v_texCoord);\
           \
           float diff = (rand(v_texCoord) - 0.5) * 0.5;\
           color.r += diff;\
