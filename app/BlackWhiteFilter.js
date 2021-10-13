ace8737ddad9a88936998b9d1f29494290457dae
Richa Deshmukh <rideshmu@microsoft.com>
2021-10-13 14:56:44 -0700

    update

--- app/BlackWhiteFilter.js
+++ app/BlackWhiteFilter.js
@@ -8,7 +8,6 @@ class BlackWhiteFilter extends ImageFilter {
       return '\
       precision highp float;\
       uniform sampler2D texture;\
-      uniform float amount = 0.5;\
       varying vec2 texCoord;\
       float rand(vec2 co) {\
           return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);\
@@ -16,7 +15,7 @@ class BlackWhiteFilter extends ImageFilter {
       void main() {\
           vec4 color = texture2D(texture, texCoord);\
           \
-          float diff = (rand(texCoord) - 0.5) * amount;\
+          float diff = (rand(texCoord) - 0.5) * 0.5;\
           color.r += diff;\
           color.g += diff;\
           color.b += diff;\
